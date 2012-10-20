12375a33c48d111c9e35793b746a69fb373f4ee8
Ben Bergstein <bennyjbergstein@gmail.com>
2012-10-20 12:08:29 -0700

    Commit new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Routevision::Application.config.secret_token = '11b8effe39233c31526f7ef4e2de0b490a537e0ad5196a352b608a98225b1e3eefe1eee6db04f3f36a35611224e92bca26057c482c2a29c7a2a36e398a3bdda5'
